800a67a6406c7bf7f011d7888ea099c83fc2418f
jeffrey.chee@yahoo.com <jeffrey.chee@yahoo.com>
2018-10-28 00:35:40 -0700

    models fixed

--- models/cat.js
+++ models/cat.js
@@ -3,27 +3,27 @@ var orm = require("../config/orm.js");
 
 var cat = {
   all: function(cb) {
-    orm.all("cats", function(res) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
+    orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("cats", objColVals, condition, function(res) {
+    orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    orm.delete("cats", condition, function(res) {
+    orm.delete("burgers", condition, function(res) {
       cb(res);
     });
   }
 };
 
-// Export the database functions for the controller (catsController.js).
-module.exports = cat;
+// Export the database functions for the controller (burgersController.js).
+module.exports = burger;
